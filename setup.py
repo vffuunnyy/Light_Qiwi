b91494d345fffa8827dfb20fb2da2fe8d9c62698
vffuunnyy <vffuunnyy@gmail.com>
2019-06-30 22:12:46 +0700

    release

--- setup.py
+++ setup.py
@@ -3,33 +3,33 @@ from setuptools import setup
 
 """
 :authors: vffuunnyy
-:license: Apache License, Version 2.0, see LICENSE file
+:license: Apache License, Version 2.0
 :copyright: (c) 2019 vffuunnyy
 """
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setup(
     name='Light_Qiwi',
-    version='1.0.1',
+    version='0.1',
     packages=['Light_Qiwi'],
     url='https://github.com/vffuunnyy/Light_Qiwi',
-    license='Apache License, Version 2.0, see LICENSE file',
+    license='Apache License, Version 2.0',
     author='vffuunnyy',
     author_email='vffuunnyy@gmail.com',
     setup_requires=['pyversion'],
     auto_version=True,
-    description='Python модуль для написания скриптов для Qiwi (qiwi.com) (API wrapper)',
+    description=u'Python модуль для написания скриптов для Qiwi (qiwi.com) (API wrapper)',
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     install_requires=['requests', 'cherrypy', 'aenum'],
     classifiers=[
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ]
 )
